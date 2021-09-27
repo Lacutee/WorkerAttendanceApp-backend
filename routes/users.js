07f0eb136f467de9efa3d12121f036efbaffd35e
Ridho Muhammad <5mblackburn7@gmail.com>
2021-09-28 01:14:22 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -68,7 +68,7 @@ router.route('/login').post((req, res) => {
         //     // res.send(token);
         //     res.status(400).json('Error : wrong password');
         // }
-        res.send(decrypt(decryptedPass));
+        res.send(decryptedPass);
     })
     .catch(err => res.status(400).json('Error: username not found '));;
 });
