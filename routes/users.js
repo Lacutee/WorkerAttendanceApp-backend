e69c9302bfee436b22c9e5a970c0d4b7f8bd70df
Ridho Muhammad <5mblackburn7@gmail.com>
2021-09-27 22:50:32 +0700

    fix login issue

--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,33 @@ let User = require('../models/users.model');
 const config = require('../config.json');
 const jwt = require('jsonwebtoken');
 
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+   }
+   
+   function decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+   }
 
 
 router.route('/register').post((req, res) => {
     const username = req.body.username;
     const name = req.body.name;
     const email = req.body.email;
-    var password = req.body.password;
+    var password = encrypt(req.body.password);
     const role = req.body.role;
-    password = crypto.createHash('sha256').update(password).digest('base64');
 
     const newUser = new User({
         username,
@@ -23,27 +41,27 @@ router.route('/register').post((req, res) => {
     });
 
     newUser.save()
-    .then(() => res.json('Success register!'))
+    .then(() => res.json(password))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/login').post((req, res) => {
     const username = req.body.username;
     var password = req.body.password;
-    password = crypto.createHash('sha256').update(password).digest('base64');
 
     User.find({'username' : username}).
     then(user => {
-        if(password == user.password) {
+        if(password == decrypt(user.password)) {
             const token = jwt.sign({ sub: user.id, role: user.role }, config.secret);
             const { password, ...userWithoutPassword } = user;
-            res.send(token);
+            
         } else{
-            const token = jwt.sign({ sub: user[0].id, role: user[0].role }, config.secret);
-            const { password, ...userWithoutPassword } = user;
-            res.send(token);
-            // res.status(400).json('Error : wrong password');
+            // const token = jwt.sign({ sub: user[0].id, role: user[0].role }, config.secret);
+            // const { password, ...userWithoutPassword } = user;
+            // res.send(token);
+            res.status(400).json('Error : wrong password');
         }
+        res.send({"token": token,"pass": decrypt(user.password)});
     })
     .catch(err => res.status(400).json('Error: username not found '));;
 });
