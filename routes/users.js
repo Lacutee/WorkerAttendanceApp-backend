0096104aa89cd89336330f751bfc645c8fc5dbe3
Ridho Muhammad <shafiraaks@gmail.com>
2021-10-01 14:49:21 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -28,11 +28,11 @@ router.route('/register').post((req, res) => {
 });
 
 router.route('/login').post((req, res) => {
-    const username = req.body.username;
+    const email = req.body.email;
     var password = req.body.password;
     password = crypto.createHash('sha256').update(password).digest('base64');
 
-    User.find({'username' : username}).
+    User.find({'email' : email}).
     then(user => {
         if(password == user[0].password) {
             const token = jwt.sign({ sub: user[0].id, role: user[0].role }, config.secret);
