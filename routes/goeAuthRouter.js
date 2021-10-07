ee2f689f5e270f4ec6fff091279a2f3f19373e4d
Ridho Muhammad <89787699+shaafiraks@users.noreply.github.com>
2021-10-07 15:27:15 +0700

    Update goeAuthRouter.js

--- routes/goeAuthRouter.js
+++ routes/goeAuthRouter.js
@@ -24,13 +24,13 @@ function getAll(req, res, next) {
         .then((users)=>{
                     res.send(users.map(
                         (user =>{
-                            [
+                            
                                 users.location,
                                 users.attendance,
                                 users.distance,
                                 users.userId,
                                 formatDateTime(user.createdAt, true)
-                            ]
+                            
                         })
                     ))
                 }
