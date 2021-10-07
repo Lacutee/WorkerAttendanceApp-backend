7dc940926ba13b78b9f694e917a6c1f269f3f20c
Ridho Muhammad <89787699+shaafiraks@users.noreply.github.com>
2021-10-07 15:02:49 +0700

    Update userForgetPassRouter.js

--- routes/userForgetPassRouter.js
+++ routes/userForgetPassRouter.js
@@ -10,8 +10,9 @@ module.exports = router;
 router.get('/forget/', question)
 
 function question(req, res, next){
+    console.log('masuk..')
     User.find()
-        .then(user => res.json({question: user.question, answer: user.answer, email: user.email, userId: user._id}))
+        .then(user => res.json(user))
         .catch(err => err.status(400).json('question not found'))
 }
 
